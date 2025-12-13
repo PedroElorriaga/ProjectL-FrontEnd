a072cd003e89ba070e9fa3672ed199b7a2a38a4e
PedroElorriaga <pedrohes2002@outlook.com>
2025-12-12 22:07:33 -0300

    feat: login route added

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Catalog from './pages/Catalog';
 import AddPerfume from './pages/AddPerfume';
+import LoginPerfume from './pages/Login';
 
 
 export default function Router() {
@@ -8,6 +9,7 @@ export default function Router() {
         <Routes>
             <Route path='/catalogo' element={<Catalog />} />
             <Route path='/adicionar' element={<AddPerfume />} />
+            <Route path='/login' element={<LoginPerfume />} />
         </Routes>
     )
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { FooterContainer, NavContentLink } from "./style";
 import { UserIcon, MagnifyingGlassIcon, BasketIcon } from "@phosphor-icons/react";
 
 export default function Footer() {
+    const navigate = useNavigate();
+
+    const handleBasketClick = () => {
+        navigate('/catalogo')
+    }
+
+    const handleUserClick = () => {
+        navigate('/login')
+    }
+
     return (
         <>
             <FooterContainer>
                 <NavContentLink>
-                    <BasketIcon size={32} />
+                    <BasketIcon size={32} onClick={handleBasketClick} />
                     <MagnifyingGlassIcon size={32} />
-                    <UserIcon size={32} />
+                    <UserIcon size={32} onClick={handleUserClick} />
                 </NavContentLink>
             </FooterContainer>
         </>
